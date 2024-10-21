5f0a4a925a0da79ad7843ae0c6d4a2f5da8cbce5
iDems <137705408+DEMSkrrt@users.noreply.github.com>
2024-10-22 05:30:18 +0800

    feat: added login and register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -18,7 +18,7 @@ function onClick() {
           :prepend-icon="
             theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
           "
-          :text="theme === 'light' ? 'Dark Mode' : 'Light Mode'"
+          text="Toggle Theme"
           slim
           @click="onClick"
         ></v-btn>
@@ -27,11 +27,11 @@ function onClick() {
       <v-main>
         <v-container>
           <v-row>
-            <v-col cols="12" md="6" class="mx-auto pt-16">
+            <v-col cols="12" md="6" class="mx-auto">
               <v-card
                 class="mx-auto"
-                prepend-icon="mdi-login"
-                subtitle="Login"
+                prepend-icon="mdi-account-plus"
+                subtitle="Register"
                 elevation="24"
               >
                 <template v-slot:title>
@@ -41,6 +41,16 @@ function onClick() {
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
                     <v-text-field
+                      label="Firstname"
+                      variant="outlined"
+                    ></v-text-field>
+
+                    <v-text-field
+                      label="Lastname"
+                      variant="outlined"
+                    ></v-text-field>
+
+                    <v-text-field
                       label="Email"
                       variant="outlined"
                     ></v-text-field>
@@ -49,8 +59,13 @@ function onClick() {
                       label="Password"
                       type="password"
                       variant="outlined"
-                    >
-                    </v-text-field>
+                    ></v-text-field>
+
+                    <v-text-field
+                      label="Password Confirmation"
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
 
                     <v-btn class="mt-2" type="submit" block>Submit</v-btn>
                   </v-form>
@@ -58,9 +73,9 @@ function onClick() {
                   <v-divider class="my-5"></v-divider>
 
                   <h5 class="text-center">
-                    Don't have an account?
-                    <router-link class="text-primary" to="/register"
-                      >Click here to Register</router-link
+                    Already have an account?
+                    <router-link class="text-primary" to="/"
+                      >Click here to Login</router-link
                     >
                   </h5>
                 </v-card-text>
