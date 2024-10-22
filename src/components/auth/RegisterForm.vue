380929b581ba4f8bb8e1f4045cf521a90e33b492
iDems <137705408+DEMSkrrt@users.noreply.github.com>
2024-10-23 00:11:00 +0800

    fix: password icons

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,21 +1,39 @@
+<script>
+export default {
+  data: () => ({
+    visible: false,
+  }),
+}
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
     <v-text-field label="Firstname" variant="outlined"></v-text-field>
 
     <v-text-field label="Lastname" variant="outlined"></v-text-field>
 
-    <v-text-field label="Email" variant="outlined"></v-text-field>
-
     <v-text-field
-      label="Password"
-      type="password"
+      label="Email"
+      prepend-inner-icon="mdi-email-outline"
       variant="outlined"
     ></v-text-field>
 
     <v-text-field
-      label="Password Confirmation"
-      type="password"
+      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="visible ? 'text' : 'password'"
+      placeholder="Enter your password"
+      prepend-inner-icon="mdi-lock-outline"
       variant="outlined"
+      @click:append-inner="visible = !visible"
+    ></v-text-field>
+
+    <v-text-field
+      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="visible ? 'text' : 'password'"
+      placeholder="Password confirmation"
+      prepend-inner-icon="mdi-lock-outline"
+      variant="outlined"
+      @click:append-inner="visible = !visible"
     ></v-text-field>
 
     <v-btn
