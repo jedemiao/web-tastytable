efd5239ed7d682f3a33b27357be5061a5c97e349
iDems <137705408+DEMSkrrt@users.noreply.github.com>
2024-10-23 05:43:06 +0800

    feat: added validators on forms

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,26 +1,47 @@
-<script>
-export default {
-  data: () => ({
-    visible: false,
-  }),
+<script setup>
+import { requiredValidator, emailValidator } from '@/utils/validators'
+import { ref } from 'vue'
+
+const visible = ref(false)
+const refVForm = ref()
+
+const formDataDefault = {
+  email: '',
+  password: '',
+}
+
+const formData = ref(formDataDefault)
+
+const onSubmit = () => {
+  // alert(formData.value.email)
+}
+
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit()
+  })
 }
 </script>
 
 <template>
-  <v-form fast-fail @submit.prevent>
+  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
     <v-text-field
+      v-model="formData.email"
       placeholder="Email"
       prepend-inner-icon="mdi-email-outline"
+      :rules="[requiredValidator, emailValidator]"
       variant="outlined"
     ></v-text-field>
 
     <v-text-field
+      v-model="formData.password"
       :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
       :type="visible ? 'text' : 'password'"
       placeholder="Enter your password"
       prepend-inner-icon="mdi-lock-outline"
       variant="outlined"
       @click:append-inner="visible = !visible"
+      :rules="[requiredValidator]"
     ></v-text-field>
 
     <v-btn
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,37 +1,86 @@
-<script>
-export default {
-  data: () => ({
-    visible: false,
-  }),
+<script setup>
+import {
+  requiredValidator,
+  emailValidator,
+  passwordValidator,
+  confirmedValidator,
+} from '@/utils/validators'
+import { supabase, formActionDefault } from '@/utils/supabase.js'
+import { ref } from 'vue'
+
+const formDataDefault = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+}
+
+const formData = ref(formDataDefault)
+const formAction = ref(formActionDefault)
+
+const ifPasswordVisible = ref(false)
+const ifPasswordConfirmVisible = ref(false)
+const refVForm = ref()
+
+const onSubmit = () => {
+  // alert(formData.value.email)
+}
+
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit()
+  })
 }
 </script>
 
 <template>
-  <v-form fast-fail @submit.prevent>
-    <v-text-field placeholder="Firstname" variant="outlined"></v-text-field>
+  <v-alert></v-alert>
 
-    <v-text-field placeholder="Lastname" variant="outlined"></v-text-field>
-
+  <v-form ref="refVForm" @submit.prevent="onSubmit">
     <v-text-field
-      placeholder="Email"
-      prepend-inner-icon="mdi-email-outline"
+      v-model="formData.firstname"
+      :rules="[requiredValidator]"
+      placeholder="Firstname"
       variant="outlined"
     ></v-text-field>
 
     <v-text-field
+      v-model="formData.lastname"
+      :rules="[requiredValidator]"
+      placeholder="Lastname"
+      variant="outlined"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="formData.email"
+      placeholder="Email"
+      prepend-inner-icon="mdi-email-outline"
+      :rules="[requiredValidator, emailValidator]"
+      variant="outlined"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="formData.password"
       :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
       :type="visible ? 'text' : 'password'"
       placeholder="Password"
       prepend-inner-icon="mdi-lock-outline"
+      :rules="[requiredValidator, passwordValidator]"
       variant="outlined"
       @click:append-inner="visible = !visible"
     ></v-text-field>
 
     <v-text-field
+      v-model="formData.password_confirmation"
       :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
       :type="visible ? 'text' : 'password'"
       placeholder="Password confirmation"
       prepend-inner-icon="mdi-lock-outline"
+      :rules="[
+        requiredValidator,
+        confirmedValidator(formData.password_confirmation, formData.password),
+      ]"
       variant="outlined"
       @click:append-inner="visible = !visible"
     ></v-text-field>
@@ -42,6 +91,8 @@ export default {
       block
       style="background-color: #d72323"
       prepend-icon="mdi-account-plus"
+      :disabled="formAction.formProcess"
+      :loading="formAction.formProcess"
       >Register</v-btn
     >
   </v-form>
